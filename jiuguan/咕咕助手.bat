b0be45b9273b6bd24026705f9b5dd970008f5796
canaan723 <qingjue723@outlook.com>
2025-10-20 18:01:58 +0800

    fix: 绕过环境变量问题

--- jiuguan/咕咕助手.bat
+++ jiuguan/咕咕助手.bat
@@ -1,10 +1,6 @@
-ï»¿@echo off
-REM
-chcp 65001 > nul
-
-REM
-powershell -ExecutionPolicy Bypass -File "%~dp0pc-st.ps1"
-
+@echo off
+%SystemRoot%\System32\chcp.exe 65001 > nul
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -File "%~dp0pc-st.ps1"
 echo.
-echo åŠ©æ‰‹å·²å…³é—­ã€‚
+echo ÖúÊÖÒÑ¹Ø±Õ¡£
 pause
