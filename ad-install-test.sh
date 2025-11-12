f9e4b944c298300b33f314046a58967bfafd4695
canaan723 <qingjue723@outlook.com>
2025-11-12 17:31:48 +0800

    test: 启动器

--- ad-install-test.sh
+++ ad-install-test.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-# 定义要下载的脚本URL和本地文件名
-SCRIPT_URL="https://gugu.qjyg.de/adtest"
+SCRIPT_URL="https://gugu.qjyg.de/test"
 FILENAME="ad-st-test.sh"
 
-# 使用 -o 来下载并强制覆盖本地文件
-echo "Downloading the latest version of ${FILENAME}..."
-curl -sSL -o "${FILENAME}" "${SCRIPT_URL}"
+echo "正在准备下载最新版的 ${FILENAME} 脚本..."
 
-# 检查curl是否成功下载
+curl -fsSL -o "${FILENAME}" "${SCRIPT_URL}"
+
 if [ $? -ne 0 ]; then
-  echo "Error: Failed to download the script."
+  echo "哎呀，下载失败了。检查下网络或者链接？"
   exit 1
 fi
 
-# 赋予执行权限
 chmod +x "${FILENAME}"
 
-echo "Script updated successfully."
+echo "脚本准备好了！马上运行..."
+echo "------------------------------------"
 
-# 如果需要，可以在这里加上自启逻辑或直接执行
-# ./$FILENAME
+./"${FILENAME}" "$@"
